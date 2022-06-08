63cb4ba24a75175893dcaf275b676968b6950ddf
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-08 13:51:49 +0300

    Creating the objects.

---
+++ SimpleSnake/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleSnake
+{
+    using SimpleSnake.GameObjects;
+    using System;
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            
+
+            ConsoleWindow.CustomizeConsole();
+
+            Point point = new Point(12, 10);
+            point.Draw('*');
+            //Console.SetCursorPosition(100, 5);
+            //Console.WriteLine("*");
+        }
+    }
+}
